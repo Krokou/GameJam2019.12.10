92a9ac5801c317b618ff623de5f21a8494f897ff
Krokou <asgeir@santi.no>
2019-10-12 15:53:18 +0200

    Test scene for 2d3d and camera movement script

---
+++ Gamejam 2019.10.12/Assets/2d3d/cameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    public float rotationSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0.0f, rotationSpeed, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0.0f, -rotationSpeed, 0.0f);
+        }
+    }
+}
