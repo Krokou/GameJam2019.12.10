ceeff74ae3c299493fe1e7a16c3f34a927a97c27
Dabble <ddabble@users.noreply.github.com>
2019-10-12 17:56:41 +0200

    Initial particle collision
    Particles collide with walls and overlap slightly with each other.

---
+++ Gamejam 2019.10.12/Assets/Scripts/LiquidParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidParticle : MonoBehaviour
+{
+    private const int PARTICLE_BORDER = 9;
+    private static bool hasConfiguredPhysics = false;
+
+    void Awake()
+    {
+        if (!hasConfiguredPhysics)
+        {
+            Physics.IgnoreLayerCollision(PARTICLE_BORDER, PARTICLE_BORDER);
+            hasConfiguredPhysics = true;
+        }
+    }
+
+    void Start() {}
+
+    void Update() {}
+}
